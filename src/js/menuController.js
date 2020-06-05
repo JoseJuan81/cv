b60466640667ac33e11bc690628e84335407aa20
josejuan81 <pe.dominguezjosejuan@gmail.com>
2020-06-04 21:54:09 -0500

    elimino node_modules

--- src/js/menuController.js
+++
@@ -1,9 +0,0 @@
-const ulProfile = document.querySelector('ul');
-
-function resetMenuItems() {
-    const li = ulProfile.children;
-    const len = li.length;
-    for (let i = 0; i < len; i+= 1) {
-        li[i].classList.remove('selected-item-profile');
-    }
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,85 +2,9 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   purge: [
-    'src/index.html',
+    'index.html',
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#E93F33',
-        primaryGray: '#F5F5F5',
-        textPrimary: '#10111e',
-        textSecondary: '#7e7e7e'
-      },
-    },
-  },
+  theme: {},
   variants: {},
-  plugins: [
-    plugin(function({ addComponents, theme }) {
-      const inputs = {
-        '.input': {
-          height: '4rem',
-          padding: '0.75rem',
-          '&:focus': {
-            outline: 'none',
-          },
-        },
-        '.edit-input': {
-          backgroundColor: 'white',
-          color: theme('colors.textPrimary'),
-        },
-        '.no-edit-input': {
-          backgroundColor: 'transparent',
-          color: theme('colors.textSecondary'),
-        },
-      };
-      const title = {
-        '.profile-section-title': {
-          color: theme('colors.primary'),
-          fontSize: '1.5rem',
-          fontWeight: 'bold',
-          marginBottom: '1rem',
-        },
-      };
-      const top = {
-        '.top-130': {
-          top: '13rem',
-        },
-        '.-top-50': {
-          top: '-5rem',
-        },
-      };
-      const btn = {
-        '.btn': {
-          color: 'white',
-          cursor: 'pointer',
-          padding: '1.2rem 3rem',
-          '&:focus': {
-            outline: 'none',
-          },
-        },
-        '.btn-save': {
-          backgroundColor: theme('colors.primary'),
-        },
-        '.btn-cancel': {
-          backgroundColor: theme('colors.textSecondary'),
-        }
-      }
-      addComponents(inputs);
-      addComponents(title);
-      addComponents(top);
-      addComponents(btn);
-    }),
-    plugin(function({ addUtilities, theme }) {
-      const newUtilities = {
-        '.flex-30': {
-          flex: '1 1 30%',
-        },
-        '.flex-70': {
-          flex: '1 1 70%',
-        }
-      };
-      addUtilities(newUtilities);
-    }),
-  ],
+  plugins: [],
 }
